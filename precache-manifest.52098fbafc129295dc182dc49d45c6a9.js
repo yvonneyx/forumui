116d1f8d45738d8b7f759b1f44e36e00f8191950
yvonneyx <zhuyuxin0627@gmail.com>
2021-03-06 15:39:58 +0100

    Updates

---
+++ precache-manifest.52098fbafc129295dc182dc49d45c6a9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "64368ff7ef8473898d0f",
+    "url": "/forumui/static/css/main.6c74d68d.chunk.css"
+  },
+  {
+    "revision": "64368ff7ef8473898d0f",
+    "url": "/forumui/static/js/main.64368ff7.chunk.js"
+  },
+  {
+    "revision": "996c1dbd0ec60fce20ad",
+    "url": "/forumui/static/css/1.4c64b038.chunk.css"
+  },
+  {
+    "revision": "996c1dbd0ec60fce20ad",
+    "url": "/forumui/static/js/1.996c1dbd.chunk.js"
+  },
+  {
+    "revision": "1b9d2b555924b85da6a0",
+    "url": "/forumui/static/js/runtime~main.1b9d2b55.js"
+  },
+  {
+    "revision": "5c11e8dba5b3d2dfee139094345c46c1",
+    "url": "/forumui/static/media/rekit-react.5c11e8db.png"
+  },
+  {
+    "revision": "dd82bb4f503351ed2cda9ecfc20d10f9",
+    "url": "/forumui/static/media/illustration.dd82bb4f.svg"
+  },
+  {
+    "revision": "ca656316fe251d75f03c5abe97bfdf69",
+    "url": "/forumui/static/media/logo.ca656316.svg"
+  },
+  {
+    "revision": "0c357fb10c498242822dea03cf2ab0f5",
+    "url": "/forumui/static/media/signup.0c357fb1.svg"
+  },
+  {
+    "revision": "6394b073f0c1074b7959719ca197c4c2",
+    "url": "/forumui/index.html"
+  }
+];
