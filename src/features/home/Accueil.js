d11a450a931595eac26ffc971581ea3101c17e4c
yvonneyx <zhuyuxin0627@gmail.com>
2021-04-16 21:48:53 +0200

    🔮 轮播图

--- src/features/home/Accueil.js
+++ src/features/home/Accueil.js
@@ -1,17 +1,16 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-import { ArticlesPopulaires, ArticlesListTab, CategoriesWidget } from './';
+import { ArticlesPopulaires, CategoriesWidget } from './';
 import { Row, Col } from 'antd';
 
-export default function Accueil() {
+export default function Accueil(props) {
   return (
     <div className="home-accueil">
-      <div>
-        <ArticlesPopulaires />
+        <ArticlesPopulaires props={props} />
         <Row>
           <Col span={18}><CategoriesWidget /></Col>
-          <Col span={6}><ArticlesListTab /></Col>
-        </Row></div>
+          <Col span={6}><div className="green"></div></Col>
+        </Row>
     </div>
   );
 };
--- src/features/home/ArticlesPopulaires.js
+++ src/features/home/ArticlesPopulaires.js
@@ -1,43 +1,106 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import PropTypes from 'prop-types';
-import { Space, Card, Avatar } from 'antd';
-const {Meta} = Card;
-export default function ArticlesPopulaires() {
+import { Space, Avatar } from 'antd';
+import _ from 'lodash';
 
-  const mockArticles = [{
-    title: 'Lorem Ipsum',
-    author: 'Lorem Ipsum',
-  }, {
-    title: 'Lorem Ipsum',
-    author: 'Lorem Ipsum',
-  }, {
-    title: 'Lorem Ipsum',
-    author: 'Lorem Ipsum',
-  }, {
-    title: 'Lorem Ipsum',
-    author: 'Lorem Ipsum',
-  }];
+export default function ArticlesPopulaires({ props }) {
+  const [idx, setIdx] = useState(0);
+  let newIdx = 0;
+
+  const mockArticles = [
+    {
+      id: 'xxx',
+      title: 'What skills and qualities do you think you will need as an engineer? ',
+      author: 'Lorem Ipsum',
+      category: 'Travel',
+    },
+    {
+      id: 'xxx',
+      title: 'What stands for a symbol of evil?',
+      author: 'Lorem Ipsum',
+      category: 'Conseils',
+    },
+    {
+      id: 'xxx',
+      title: 'Who discovered the principle of the photoelectric effect?',
+      author: 'Lorem Ipsum',
+      category: 'Arts',
+    },
+    {
+      id: 'xxx',
+      title: 'How many autistic children and adults are there in the UK? ',
+      author: 'Lorem Ipsum',
+      category: 'Photographie',
+    },
+    {
+      id: 'xxx',
+      title:
+        ' How long has the NAS worked / been working with autism...How long has the NAS worked / been working with autism..How long has the NAS worked / been working with autism',
+      author: 'Lorem Ipsum',
+      category: 'Sports',
+    },
+    {
+      id: 'xxx',
+      title: 'Lorem Ipsum',
+      author: 'Lorem Ipsum',
+      category: 'Lorem Ipsum',
+    },
+    {
+      id: 'xxx',
+      title: 'Who discovered the principle of the photoelectric effect?',
+      author: 'Lorem Ipsum',
+      category: 'Arts',
+    },
+  ];
+
+  const sliceOfArticles = (data, n, idx) => {
+    let length = data.length;
+    let rest = length - idx;
+    let newArr = [];
+    if (rest > 0 && rest < n) {
+      newArr = [...data.slice(length - rest, length), ...data.slice(0, n - rest)];
+    } else if (rest >= n) {
+      newArr = data.slice(idx, idx + n);
+    }
+    return newArr;
+  };
 
   return (
     <div className="home-articles-populaires">
+      <div className="home-articles-populaires-title">Les plus populaires</div>
+      <div
+        className="basic-arrow left-arrow"
+        onClick={() => {
+          idx === 0 ? (newIdx = mockArticles.length - 1) : (newIdx = idx - 1);
+          setIdx(newIdx);
+        }}
+      ></div>
       <Space size="middle">
-        {mockArticles.map((item) => {
+        {_.map(sliceOfArticles(mockArticles, 4, idx), item => {
           return (
-            <Card
-               className="home-articles-populaires-card"
-              hoverable
-              cover={<img src="https://source.unsplash.com/random" alt="avatar" className="img"/>}
-              bordered={false}
-            >
-              <Meta avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />} title={item.title} 
-              description={`By ${item.author}`} />
-            </Card>
-          )
+            <div className="home-articles-populaires-item" onClick={() => { props.history.push(`/post/${item.id}`) }} >
+              <div className="item-category">{item.category}</div>
+              <div className="item-title">{item.title} </div>
+              <div className="item-author">
+                <div className="item-author-txt">Publié par </div>
+                <br />
+                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                <div className="item-author-name">{item.author}</div>
+              </div>
+            </div>
+          );
         })}
       </Space>
+      <div
+        className="basic-arrow right-arrow"
+        onClick={() => {
+          idx === mockArticles.length - 1 ? (newIdx = 0) : (newIdx = idx + 1);
+          setIdx(newIdx);
+        }}
+      ></div>
     </div>
   );
-};
+}
 
 ArticlesPopulaires.propTypes = {};
 ArticlesPopulaires.defaultProps = {};
